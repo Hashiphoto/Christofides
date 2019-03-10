e7544ddeab8471a99ac5f84ded28df279b651480
Trent Hashimoto <t.hashiphoto@gmail.com>
2019-03-09 22:37:04 -0800

    Add graph support for weight and undirected

--- Christofides/Graph.cs
+++ Christofides/Graph.cs
@@ -6,21 +6,51 @@ namespace Christofides
 {
     class Graph
     {
-        Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
+        Dictionary<int, List<Edge>> graph = new Dictionary<int, List<Edge>>();
 
-        public void AddEdge(int v1, int v2)
+        public void AddUndirectedEdge(int v1, int v2, int weight)
         {
-            if(graph[v1] == null)
-            {
-                graph[v1] = new List<int> { v2 };
-                return;
-            }
-            graph[v1].Add(v2);
+            AddDirectedEdge(v1, v2, weight);
+            AddDirectedEdge(v2, v1, weight);
         }
 
-        public bool Connected(int v1, int v2)
+        private void AddDirectedEdge(int source, int dest, int weight)
         {
-            return graph[v1] == null || graph[v1].Contains(v2);
+            Edge newEdge = new Edge(dest, weight);
+            if (graph[source] == null)
+            {
+                graph[source] = new List<Edge> { newEdge };
+                return;
+            }
+            graph[source].Add(newEdge);
+        }
+
+        // Returns the weight of the edge between v1 and v2
+        // If there is no edge, it returns -1
+        public int GetWeight(int v1, int v2)
+        {
+            if (graph[v1] == null)
+                return -1;
+            for(int i = 0; i < graph[v1].Count; i++)
+            {
+                if(graph[v1][i].destination == v2)
+                {
+                    return graph[v1][i].weight;
+                }
+            }
+            return -1;
+        }
+    }
+
+    class Edge
+    {
+        public int destination;
+        public int weight;
+
+        public Edge(int destination, int weight)
+        {
+            this.destination = destination;
+            this.weight = weight;
         }
     }
 }
