71c88ec389c52b50db7ee06d5f38c6a70da1cea8
Trent Hashimoto <t.hashiphoto@gmail.com>
2019-03-09 22:13:54 -0800

    Initial commit

---
+++ Christofides/Graph.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Christofides
+{
+    class Graph
+    {
+        Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
+
+        public void AddEdge(int v1, int v2)
+        {
+            if(graph[v1] == null)
+            {
+                graph[v1] = new List<int> { v2 };
+                return;
+            }
+            graph[v1].Add(v2);
+        }
+
+        public bool Connected(int v1, int v2)
+        {
+            return graph[v1] == null || graph[v1].Contains(v2);
+        }
+    }
+}
---
+++ Christofides/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Christofides
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Graph graph = new Graph();
+
+            // Fill graph with data
+
+            // Find minimum spanning tree T
+
+            // Find minimum matching M for the odd-degree vertices in T
+
+            // Add M to T
+
+            // Find a Euler tour
+
+            // Cut short
+        }
+
+        // Find a minimum spanning tree using Prim's algorithm
+        //Graph PrimMST(Graph graph)
+        //{
+
+        //}
+    }
+}
